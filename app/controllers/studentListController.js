58346114de627072c47a0d8ae7b75b08f22bd830
Kulgar <kulgar@ct2c.fr>
2017-02-05 15:57:14 +0100

    With component

---
+++ app/controllers/studentListController.js
@@ -0,0 +1,17 @@
+angular.module("studentApp").controller("studentListController", function studentListController() {
+  this.students = [{
+    "lastname": "Mendy",
+    "firstname": "Bénédicte",
+    "age": 30,
+    "gender": "F",
+    "address":"31 rue Jacques Gabriel 33160 Saint Médard en Jalles",
+    "id": 0
+  }, {
+      "lastname": "Delgado  ",
+      "firstname": "  Elsa",
+      "age": 43,
+      "gender": "F",
+      "address":"1 place du Gart  33610   CESTAS",
+      "id": 1
+  }]
+})
--- app/studentApp.js
+++ app/studentApp.js
@@ -1,22 +1,2 @@
-let studentApp = angular.module('studentApp', []);
-
-function studentListController($scope) {
-  $scope.greetings = "Hello World"
-  $scope.students = [{
-    "lastname": "Mendy",
-    "firstname": "Bénédicte",
-    "age": 30,
-    "gender": "F",
-    "address":"31 rue Jacques Gabriel 33160 Saint Médard en Jalles",
-    "id": 0
-  }, {
-      "lastname": "Delgado  ",
-      "firstname": "  Elsa",
-      "age": 43,
-      "gender": "F",
-      "address":"1 place du Gart  33610   CESTAS",
-      "id": 1
-  }]
-}
-
-studentApp.controller("studentListController", studentListController)
+'use strict'
+angular.module('studentApp', []);
