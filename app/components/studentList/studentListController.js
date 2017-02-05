b32f58212d6dbd6288d38c81f0daaeb9af8313f9
Kulgar <kulgar@ct2c.fr>
2017-02-05 16:21:52 +0100

    Added  and injected it to prevent bug when minify

--- app/components/studentList/studentListController.js
+++ app/components/studentList/studentListController.js
@@ -1,19 +1,9 @@
-angular.module("studentList").controller("studentListController", function studentListController() {
-  this.students = [{
-    "lastname": "Mendy",
-    "firstname": "Bénédicte",
-    "age": 30,
-    "gender": "F",
-    "address":"31 rue Jacques Gabriel 33160 Saint Médard en Jalles",
-    "id": 0
-  }, {
-      "lastname": "Delgado  ",
-      "firstname": "  Elsa",
-      "age": 43,
-      "gender": "F",
-      "address":"1 place du Gart  33610   CESTAS",
-      "id": 1
-  }]
-
+angular.module("studentList").controller("studentListController", ["$http", function studentListController($http) {
   this.orderProp = 'age'
-})
+
+  $http.get("/students.json").then((response) => {
+    this.students = response.data
+  })
+
+
+}])
