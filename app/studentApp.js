000b3e0c48e0389fa230f29f4814f77423a2a67d
Kulgar <kulgar@ct2c.fr>
2017-02-24 19:04:38 +0100

    Introducing routes

--- app/studentApp.js
+++ app/studentApp.js
@@ -1,65 +1,15 @@
 'use strict'
 angular.module('studentApp', [
-  'studentList'
-]);
+  'studentList',
+  'ngRoute'
+]).config(['$locationProvider', '$routeProvider',
+  function config($locationProvider, $routeProvider) {
+    $locationProvider.hashPrefix('!');
 
-
-
-(function() {
-  console.log("Hello");
-  let studentsJSON;
-
-  // equivalent: get("/students.json").then(JSON.parse)
-  getJSON("/students.json").then(function(students) {
-    studentsJSON = students;
-
-    return Promise.all(
-      students.map(function(student) {
-        return getJSON(`/details_info_${student.id}.json`);
-      })
-    )
-  }).then(function(students){
-    console.log(students)
-    students.forEach(function(student){
-      console.log(student)
-    })
-  }).catch(function(error) {
-    console.log("Failed ! ", error)
-  })
-
-})()
-
-function getJSON(url) {
-  return get(url).then(JSON.parse);
-}
-
-function get(url) {
-  // Return a new promise.
-  return new Promise(function(resolve, reject) {
-    // Do the usual XHR stuff
-    var req = new XMLHttpRequest();
-    req.open('GET', url);
-
-    req.onload = function() {
-      // This is called even on 404 etc
-      // so check the status
-      if (req.status == 200) {
-        // Resolve the promise with the response text
-        resolve(req.response);
-      }
-      else {
-        // Otherwise reject with the status text
-        // which will hopefully be a meaningful error
-        reject(Error(req.statusText));
-      }
-    };
-
-    // Handle network errors
-    req.onerror = function() {
-      reject(Error("Network Error"));
-    };
-
-    // Make the request
-    req.send();
-  });
-}
+    $routeProvider.
+      when('/students', {
+        template: '<student-list></student-list>'
+      }).
+      otherwise('/students')
+  }
+])
