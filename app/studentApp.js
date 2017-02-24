197bc1e3d07653cab99e871fae398a81c66a35f4
Kulgar <kulgar@ct2c.fr>
2017-02-24 17:05:50 +0100

    2 chained requests without promise

--- app/studentApp.js
+++ app/studentApp.js
@@ -2,3 +2,25 @@
 angular.module('studentApp', [
   'studentList'
 ]);
+
+
+
+(function() {
+  console.log("Hello");
+  let req1 = new XMLHttpRequest();
+  req1.open("GET", "/students.json");
+  req1.onload = function(){
+    if (req1.status === 200) {
+      let req2 = new XMLHttpRequest();
+      req2.open("GET", "/details_info_1.json");
+      req2.onload = function(){
+        if (req2.status === 200) {
+          console.log(req1.response)
+          console.log(req2.response)
+        }
+      }
+      req2.send();
+    }
+  }
+  req1.send()
+})()
