c80ce0331eefd273ca0b0c9822917b4c944dbe36
Kulgar <kulgar@ct2c.fr>
2017-02-24 17:11:58 +0100

    A simple promise

--- app/studentApp.js
+++ app/studentApp.js
@@ -7,20 +7,41 @@ angular.module('studentApp', [
 
 (function() {
   console.log("Hello");
-  let req1 = new XMLHttpRequest();
-  req1.open("GET", "/students.json");
-  req1.onload = function(){
-    if (req1.status === 200) {
-      let req2 = new XMLHttpRequest();
-      req2.open("GET", "/details_info_1.json");
-      req2.onload = function(){
-        if (req2.status === 200) {
-          console.log(req1.response)
-          console.log(req2.response)
-        }
-      }
-      req2.send();
-    }
-  }
-  req1.send()
+  get("/students.json").then(function(response) {
+    console.log("Success ! ", response)
+  }, function(error) {
+    console.log("Failed ! ", error)
+  })
+
 })()
+
+function get(url) {
+  // Return a new promise.
+  return new Promise(function(resolve, reject) {
+    // Do the usual XHR stuff
+    var req = new XMLHttpRequest();
+    req.open('GET', url);
+
+    req.onload = function() {
+      // This is called even on 404 etc
+      // so check the status
+      if (req.status == 200) {
+        // Resolve the promise with the response text
+        resolve(req.response);
+      }
+      else {
+        // Otherwise reject with the status text
+        // which will hopefully be a meaningful error
+        reject(Error(req.statusText));
+      }
+    };
+
+    // Handle network errors
+    req.onerror = function() {
+      reject(Error("Network Error"));
+    };
+
+    // Make the request
+    req.send();
+  });
+}
